f4021890a06db00f7eeffa09ae9d16c7cadfad21
piyushgith <piyushprasad2007@gmail.com>
2019-11-26 23:15:02 +0530

    Revert the change

--- src/main/java/com/example/petstore/ServletInitializer.java
+++ src/main/java/com/example/petstore/ServletInitializer.java
@@ -3,12 +3,11 @@ package com.example.petstore;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer /* extends SpringBootServletInitializer */ {
+public class ServletInitializer extends SpringBootServletInitializer {
 
-	/*
-	 * @Override protected SpringApplicationBuilder
-	 * configure(SpringApplicationBuilder application) { return
-	 * application.sources(PetstoreApplication.class); }
-	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PetstoreApplication.class);
+	}
 
 }
